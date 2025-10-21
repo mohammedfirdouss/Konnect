501dc35f51441259f1925fa4326c8c5456127834
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-10-21 16:19:03 +0000

    remove unused PostgreSQL dialect import from gamification migration script

--- alembic/versions/add_gamification_and_supporting_tables.py
+++ alembic/versions/add_gamification_and_supporting_tables.py
@@ -7,7 +7,6 @@ Create Date: 2024-01-01 12:00:00.000000
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'add_gamification_tables'
